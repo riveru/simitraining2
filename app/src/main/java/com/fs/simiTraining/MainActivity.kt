5138e4b4d0bb24e3619c17d33d61ea67c6eafb6b
rhrvelez <riveru@gmail.com>
2025-10-19 16:11:12 -0600

    git init

---
+++ app/src/main/java/com/fs/simiTraining/MainActivity.kt
@@ -0,0 +1,76 @@
+package com.fs.simiTraining
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.fs.simiTraining.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    lateinit var quizModelList : MutableList<QuizModel>
+    lateinit var adapter: QuizListAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        quizModelList = mutableListOf()
+        checkFirebaseConnection()
+        getDataFromFirebase()
+    }
+
+    private fun checkFirebaseConnection() {
+        val connectedRef = FirebaseDatabase.getInstance().getReference(".info/connected")
+        connectedRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val connected = snapshot.getValue(Boolean::class.java) ?: false
+                if (connected) {
+                    Log.d("FirebaseConnection", "Conectado a Firebase.")
+                } else {
+                    Log.d("FirebaseConnection", "Desconectado de Firebase.")
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.w("FirebaseConnection", "El listener fue cancelado", error.toException())
+            }
+        })
+    }
+
+    private fun setupRecyclerView(){
+        binding.progressBar.visibility = View.GONE
+        adapter = QuizListAdapter(quizModelList)
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.adapter = adapter
+    }
+
+    private fun getDataFromFirebase(){
+        binding.progressBar.visibility = View.VISIBLE
+        FirebaseDatabase.getInstance("https://simitraining-default-rtdb.firebaseio.com/").reference
+            .get()
+            .addOnSuccessListener { dataSnapshot->
+                if(dataSnapshot.exists()){
+                    for (snapshot in dataSnapshot.children){
+                        val quizModel = snapshot.getValue(QuizModel::class.java)
+                        if (quizModel != null) {
+                            quizModelList.add(quizModel)
+                        }
+                    }
+                }
+                setupRecyclerView()
+            }
+            .addOnFailureListener{
+                Log.e("MainActivity", "Error al obtener datos de Firebase", it)
+                binding.progressBar.visibility = View.GONE
+            }
+
+
+    }
+}
